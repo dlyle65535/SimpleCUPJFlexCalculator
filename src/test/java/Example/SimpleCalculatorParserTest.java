5a54f8168508b8d73402abec1cdd86283be8ddcc
David M. Lyle <dlyle65535@gmail.com>
2013-03-16 20:10:11 -0400

    Changed getResults to use interface and added a few more tests.

--- src/test/java/Example/SimpleCalculatorParserTest.java
+++ src/test/java/Example/SimpleCalculatorParserTest.java
@@ -1,7 +1,8 @@
 package Example;
 
 import java.io.ByteArrayInputStream;
-import java.util.Vector;
+import java.util.List;
+import static junit.framework.Assert.assertEquals;
 import junit.framework.TestCase;
 
 public class SimpleCalculatorParserTest extends TestCase {
@@ -21,7 +22,23 @@ public class SimpleCalculatorParserTest extends TestCase {
     }
     
     public void testCanAddTwoNumbers() throws Exception{
-        Vector<Integer> v = Parser.getResults(new ByteArrayInputStream("4+4;".getBytes()));
-        assertEquals("Should parse the expression and return 8", (Integer)8,v.get(0));
+        List<Integer> results = Parser.getResults(new ByteArrayInputStream("4+4;".getBytes()));
+        assertEquals("Should parse the expression and return 8", (Integer)8,results.get(0));
     }
+    
+    public void testCanMultiplyTwoNumbers() throws Exception{
+        List<Integer> results = Parser.getResults(new ByteArrayInputStream("4*4;".getBytes()));
+        assertEquals("Should parse the expression and return 16", (Integer)16,results.get(0));   
+    }
+    
+    public void testSingleNumber() throws Exception{
+        List<Integer> results = Parser.getResults(new ByteArrayInputStream("57;".getBytes()));
+        assertEquals("Should parse the expression and return 57", (Integer)57,results.get(0));           
+    }
+
+    public void testParenthesis() throws Exception{
+        List<Integer> results = Parser.getResults(new ByteArrayInputStream("4*(1+4);".getBytes()));
+        assertEquals("Should parse the expression and return 20", (Integer)20,results.get(0));  
+    }
+
 }
